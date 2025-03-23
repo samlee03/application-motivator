0b0ec32fb2eb0a252445735faf191a0fcfa611b6
Sam Lee <sam.lee03@myhunter.cuny.edu>
2025-03-22 21:39:09 -0400

    add /jobs endpoint

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 app.use(cors())
@@ -7,11 +11,45 @@ app.use(express.json())
 
 const port = 5000
 
+const jobs = []
 app.get("/", (req, res) => {
     res.send({
         "message": "hello world"
     })
 })
+
+app.get("/jobs", async(req, res) => {
+    const client = new MongoClient(process.env.MONGO_URI);
+
+    client.connect()
+    const db = client.db('db');
+    const jobs = db.collection('jobs');
+
+    const documents = await jobs.find({}).toArray();
+    res.send({
+        // "test": "test"
+        "documents": documents
+
+    })
+})
+
+
+// For the purpose of adding jobs to the DB
+// app.get("/add_jobs", async (req, res) => {
+//     const client = new MongoClient(process.env.MONGO_URI)
+//     try {
+//         await client.connect();
+//         const database = client.db('db');
+//         const collection = database.collection('jobs');
+
+//         const result = await collection.insertMany(jobs);
+//         console.log(`Inserted ${result.insertedCount} documents.`);
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// })
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
